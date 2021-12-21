c96b21915f0b1daf137de912cf046af227c6415c
hlly352 <hlly352@gmail.com>
2021-12-21 16:14:43 +0800

    自定义函数

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -33,9 +33,9 @@ class NewsController extends Controller
     	$rs = DB::select('select * from sc_name');
     	$rs = DB::select('select * from sc_name where id= ?',[1,3]);
     	
-    	foreach($rs as $k=>$v){
-    		dump($v->name);
-    	}
+    	// foreach($rs as $k=>$v){
+    	// 	dump($v->name);
+    	// }
     	//插入数据
     	//$res = DB::insert('insert into sc_name(id,name) values(4,"feng")');
     	//dump($res);
@@ -57,8 +57,38 @@ class NewsController extends Controller
 
 
     	//查询构造器
-    	//get获取表里所有的数据
-    	$rs1 = DB::table('sc_name')->get();
-    	dump($rs1);
+    	//get获取表里所有的数据cccccccccccccccccccccccccccccccccccccc
+    	// $rs1 = DB::table('sc_name')->get();
+    	// dump($rs1);
+    	//添加数据
+    	//$rs = DB::table('sc_name')->insert([
+    		// 	'name'=>'川哥'
+    		// ]);
+    	//多条数据，二维数组
+    	// $rs = DB::table('sc_name')->insert([
+    	// 		['name'=>'川哥哥'],
+    	// 		['name'=>'裁缝e']
+    	// 	]);
+    	//获取最后插入id
+    	// $rs = DB::table('sc_name')->insertGetId(['name'=>'新名字']);
+    	//更新
+    	// $rs = DB::table('sc_name')->where('id','<','2')->update(['name'=>'change']);
+    	//查询第一条
+    	//$rs = DB::table('sc_name')->first();
+    	//查询某个字段
+    	//$rs = DB::table('sc_name')->where('id','2')->value('name');
+    	//查询一列值
+    	//$rs = DB::table('sc_name')->pluck('name');
+    	//设置字段的方式
+    	//$rs = DB::table('sc_name')->select('name')->get();
+    	//$rs = DB::table('sc_name')->where('name','like','%a%')->get();
+    	//$rs = DB::table('sc_name')->whereBetween('id',[1,3])->get();
+    	//排序
+    	//orderBy('id','desc');
+    	//分页
+    	
+    	//dump($rs);
+    	func();
+
     }
 }
